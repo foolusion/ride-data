0a7b6fe8ed3746dcd891a1cdc1a27d43dcf7630c
Andrew O'Neill <foolusion@gmail.com>
2013-08-19 22:15:23 -0400

    added license to ridefile.go

--- ridefile/ridefile.go
+++ ridefile/ridefile.go
@@ -1,3 +1,6 @@
+// Copyright 2013 Andrew O'Neill. All rights reserved.  Use of this file is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
 // Package ridefile implements a simple ride file interface for reading
 // exported ride data.
 package ridefile
